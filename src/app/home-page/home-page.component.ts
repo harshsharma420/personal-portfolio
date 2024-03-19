753fbd2f7ab26cc10ff15bac45b4c9bbc21938db
Harsh Sharma <sharmaharsh555@gmail.com>
2024-03-19 19:13:51 +0530

    added dynamically changing background-color to text

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnDestroy,OnInit {
+  interval: any;
+  i=0;
+  constructor() {
 
+  }
+  ngOnInit(){
+
+    this.interval = setInterval(() => {
+      console.log("Interval");
+      this.i = Math.floor(Math.random() * 4);
+    }, 500);
+  }
+  ideasStyle() {
+    let color = [
+      {
+        "background-color": "pink"
+      },
+      {
+        "background-color": "red",
+        "color":"white"
+      },
+      {
+        "background-color": "orange",
+        "color":"black"
+      },
+      {
+        "background-color": "lavendar",
+        "color":"white"
+      },
+    ];
+    return color[this.i];
+  }
+
+  
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+    console.log("onDestroy");
+  }
 }
